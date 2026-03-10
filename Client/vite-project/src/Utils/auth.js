c609f5afe556405cdd3362fa9a81f2eb9ab5455c
Rameshmanohar18 <rameshbradmandgl@gmail.com>
2026-03-10 16:58:16 +0530

    a

--- Client/vite-project/src/Utils/auth.js
+++ Client/vite-project/src/Utils/auth.js
@@ -1,16 +1,12 @@
-// src/utils/auth.js
 
-// Save JWT token to localStorage
 export const saveToken = (token) => {
   localStorage.setItem("token", token);
 };
 
-// Retrieve JWT token from localStorage
 export const getToken = () => {
   return localStorage.getItem("token");
 };
 
-// Remove JWT token (logout)
 export const removeToken = () => {
   localStorage.removeItem("token");
 };
